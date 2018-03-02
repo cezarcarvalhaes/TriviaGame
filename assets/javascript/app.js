782da264aac15ff2c7f38ab94f46d3fb144e6ab5
Cezar Carvalhaes <cezarcarvalhaes@Cezars-CIS-MacBook-Air.local>
2018-03-02 12:56:41 -0500

    created timer functions and timer display.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,10 @@
 $(document).ready (function() {
 
+//Variable that will hold our interval for question timer
+var showQuestion;
+
+var timerRunning = false;
+
 var questionArr = [
     {question:"When was the war of 1812?", 
       a:"1912",
@@ -23,23 +28,32 @@ var questionArr = [
 
 
 //Start Game
-    $(".start").on("click",function(){
-        $(".start").remove();
-        displayQuestion();
-    });
-
-    function displayQuestion() {
-        //Prints random question and answer choices
+    $(".start").on("click",startGame);
+    
+    //Sets interval on main game function
+    function startGame() {
+    nextQuestion();
+    showQuestion = setInterval(timeUp,1000 * 30);
+    };
+    //Main function driving the game
+    function nextQuestion() {
+        //Empties any previous content in game interface
+        $(".interface").empty();
+        //Selects random question and answer choices
         var r = Math.floor(Math.random() * (questionArr.length));
         
-        //Displays Question and shuffled answer choices:
+        //Displays Question, timer and shuffled answer choices:
         $(".interface").append('<div class="question"><p>'+ questionArr[r].question+'</p></div>');
+        $(".question").append('<div id="time-left"><p>Time left: <span id="timer">30</span> seconds</p>')
+        //Starts countdown
+        startCountDown();
         var choices = shuffleChoices();
 
         for (i=0;i<choices.length;i++) {
             console.log(i,choices[i])
             choices[i]();
         }
+        
         //Array of functions so we can randomize answer choices; Fisher-Yates algorithm.
         function shuffleChoices() {
             var answerChoices=[choice1,choice2,choice3,choice4];
@@ -62,7 +76,8 @@ var questionArr = [
             console.log(answerChoices);
             return answerChoices;
         }
-
+        
+        //These functions append our answer choices below our question. Get shuffled in shuffleChoices().
         function choice1(){
         $(".interface").append('<div class="choices"><p>'+ questionArr[r].a +'</p></div>')
         };
@@ -85,7 +100,6 @@ var questionArr = [
                 console.log("Correct!");
                 questionArr.splice(r, 1); // Remove this question from game
                 rightAnswer();
-                setTimeout(nextQuestion,4000);
             }
             else {
                 $(this).addClass('wrong');
@@ -94,30 +108,56 @@ var questionArr = [
                 console.log(this.text)
                 questionArr.splice(r, 1); // Remove this question from game
                 wrongAnswer();
-                setTimeout(nextQuestion,4000);
             }
         });
     };
-
-    function nextQuestion() {
-        $(".interface").empty();
-        displayQuestion();
-    }
 
     function rightAnswer() {
         $(".interface").empty()
         var r = Math.floor(Math.random() * (correctImgArr.length));
         $(".interface").html('<img src="'+ correctImgArr[r] +'" class="giphy" />')
         $(".interface").prepend("<h2>Correct!</h2>")
+        clearInterval(showQuestion);
+        stopCountDown();
+        setTimeout(startGame,3000);
     }
 
     function wrongAnswer() {
         $(".interface").empty()
         var r = Math.floor(Math.random() * (wrongImgArr.length));
         $(".interface").html('<img src="'+ wrongImgArr[r] +'" class="giphy" />')
-        $(".interface").prepend("<h2>Correct!</h2>")   
+        $(".interface").prepend("<h2>Incorrect!</h2>")
+        clearInterval(showQuestion);
+        stopCountDown();
+        setTimeout(startGame,3000);
+    }
+
+    function timeUp() {
+        $(".interface").empty()
+        var r = Math.floor(Math.random() * (wrongImgArr.length));
+        $(".interface").html('<img src="'+ wrongImgArr[r] +'" class="giphy" />')
+        $(".interface").prepend("<h2>Time's Up!</h2>")
+        clearInterval(showQuestion);
+        stopCountDown();
+        setTimeout(startGame,3000);
+    }
+
+    function startCountDown() {
+        if (!timerRunning) {
+        setInterval(countDown,1000);
+        timerRunning = true;
+        }
+    }
+    function stopCountDown() {
+        clearInterval(countDown);
+    }
+
+    function countDown() {
+        var timeLeft = parseInt($("#timer").text());
+        timeLeft--;
+        $("#timer").html(timeLeft);
     }
 
     var correctImgArr = ["assets/images/bill-murray.gif","assets/images/kramer.gif","assets/images/obama.gif", "assets/images/so-smart.gif","assets/images/thats-it.gif","assets/images/think.gif","assets/images/usmart.gif","assets/images/walter-white.gif"]
-    var wrongImgArr = ["assets/images/3rd-rock.gif","assets/images/big-show.gif","assets/images/dr-cox.gif","assets/images/nope.gif","assets/images/u-thought-wrong.gif","assets/images/ur-wrong.gif","assets/images/wallstreet.gif","assets/images/dog-fail.gif",]
+    var wrongImgArr = ["assets/images/3rd-rock.gif","assets/images/big-show.gif","assets/images/dr-cox.gif","assets/images/nope.gif","assets/images/u-thought-wrong.gif","assets/images/ur-wrong.gif","assets/images/wallstreet.gif",]
 });
